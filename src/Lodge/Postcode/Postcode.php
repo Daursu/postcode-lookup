6c2f0087d654293dffd4615bb1e3ba913ec11e12
Dan Ursu <10077734@brookes.ac.uk>
2013-07-06 13:07:01 +0100

    improved the way the results are being parsed

--- src/Lodge/Postcode/Postcode.php
+++ src/Lodge/Postcode/Postcode.php
@@ -14,15 +14,59 @@ class Postcode {
 		$lng = $json->results[0]->geometry->location->lng;
 
 		// A second call will now retrieve the address
-		$address_url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $lat . ',' . $lng . '&sensor=false';
+		$address_url  = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $lat . ',' . $lng . '&sensor=false';
 		$address_json = json_decode(file_get_contents($address_url));
 		$address_data = $address_json->results[0]->address_components;
-		// $street = str_replace('Dr', 'Drive', $address_data[1]->long_name);
-		$street = (isset($address_data[1]->long_name)) ? $address_data[1]->long_name : '';
-		$town = (isset($address_data[2]))? $address_data[2]->long_name : '';
-		$county = (isset($address_data[3])) ? $address_data[3]->long_name : '';
 
-		$array = array('postcode' => $postcode, 'street' => $street, 'town' => $town, 'county' => $county, 'latitude' => $lat, 'longitude' => $lng);
+		// Initialize all values as empty
+		$street_number = '';
+		$street        = '';
+		$sublocality   = '';
+		$town          = '';
+		$county        = '';
+		$country       = '';
+
+		// Process the return
+		foreach ($address_data as $value) {
+
+			if (array_search('street_number', $value->types) !== FALSE)
+			{
+				$street_number = $value->long_name;
+			}
+			elseif (array_search('route', $value->types) !== FALSE)
+			{
+				$street = $value->long_name;
+			}
+			elseif (array_search('sublocality', $value->types) !== FALSE)
+			{
+				$sublocality = $value->long_name;
+			}
+			elseif (array_search('locality', $value->types) !== FALSE)
+			{
+				$town = $value->long_name;
+			}
+			elseif (array_search('administrative_area_level_2', $value->types) !== FALSE)
+			{
+				$county = $value->long_name;
+			}
+			elseif (array_search('country', $value->types) !== FALSE)
+			{
+				$country = $value->long_name;
+			}
+		}
+
+		// Prepare the response
+		$array = array(
+			'postcode'      => $postcode,
+			'street_number' => $street_number,
+			'street'        => $street,
+			'sublocality'   => $sublocality,
+			'town'          => $town,
+			'county'        => $county,
+			'latitude'      => $lat,
+			'longitude'     => $lng
+		);
+
 		return $array;
 	}
 
