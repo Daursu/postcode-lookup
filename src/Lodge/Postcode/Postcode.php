bf6d696f243680ebca0d0fb46ac97e4c2ea2ec66
Dan Ursu <10077734@brookes.ac.uk>
2015-01-30 22:50:48 +0000

    converted spaces to tabs

--- src/Lodge/Postcode/Postcode.php
+++ src/Lodge/Postcode/Postcode.php
@@ -10,79 +10,78 @@ class Postcode {
 		// Retrieve the latitude and longitude
 		$url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' . $search_code . '&sensor=false';
 
-        // If Google Maps API fails, catch it and throw a better error
-        try
-        {
-            $json = json_decode(file_get_contents($url));
-        }
-        catch(\Exception $e)
-        {
-            throw new ServiceUnavailableException;
-        }
+		// If Google Maps API fails, catch it and throw a better error
+		try
+		{
+			$json = json_decode(file_get_contents($url));
+		}
+		catch(\Exception $e)
+		{
+			throw new ServiceUnavailableException;
+		}
 
-        if(!empty($json->results))
-        {
-            $lat = $json->results[0]->geometry->location->lat;
-            $lng = $json->results[0]->geometry->location->lng;
+		if(!empty($json->results))
+		{
+			$lat = $json->results[0]->geometry->location->lat;
+			$lng = $json->results[0]->geometry->location->lng;
 
-    		return array(
-	    		'latitude'  => $lat,
-		    	'longitude' => $lng
-		    );
-        }
+			return array(
+				'latitude'  => $lat,
+				'longitude' => $lng
+			);
+		}
 
-        // We must have nothing if we've got here
-        return array();
-
+		// We must have nothing if we've got here
+		return array();
 	}
 
-    public function mutatePostcode($postcode)
-    {
-        // Ensure the postcode is all upper case with no spaces
-        return preg_replace('/ /', '', strtoupper($postcode));
-    }
+	public function mutatePostcode($postcode)
+	{
+		// Ensure the postcode is all upper case with no spaces
+		return preg_replace('/ /', '', strtoupper($postcode));
+	}
 
 	public function lookup($postcode)
-    {
-        // Mutate the postcode
-        $postcode = $this->mutatePostcode($postcode);
+	{
+		// Mutate the postcode
+		$postcode = $this->mutatePostcode($postcode);
 
 		// Sanitize the postcode:
 		$coords = $this->getCoordinates($postcode);
 
-        // If empty, we must have no results
-        if(empty($coords))
-            return array();
+		// If empty, we must have no results
+		if(empty($coords))
+			return array();
 
 		// A second call will now retrieve the address
 		$address_url  = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $coords['latitude'] . ',' . $coords['longitude'] . '&sensor=false';
 		$address_json = json_decode(file_get_contents($address_url));
 
-        // The correct result is not always the first one, so loop through results here
-        foreach($address_json->results as $current_address)
-        {
-            foreach($current_address->address_components as $component)
-            {
-                // If this address_component is a postcode and matches...
-                if(array_search('postal_code', $component->types) !== FALSE &&
-                   $postcode == $this->mutatePostcode($component->long_name))
-                {
-                    $address_data = $current_address->address_components;
-                    break 2;
-                }
-            }
-        }
+		// The correct result is not always the first one, so loop through results here
+		foreach($address_json->results as $current_address)
+		{
+			foreach($current_address->address_components as $component)
+			{
+				// If this address_component is a postcode and matches...
+				if(array_search('postal_code', $component->types) !== FALSE &&
+				   $postcode == $this->mutatePostcode($component->long_name))
+				{
+					$address_data = $current_address->address_components;
+					break 2;
+				}
+			}
+		}
 
-        // If no exact match found, use the first as a closest option
-        if(!isset($address_data) &&
-           !empty($address_json->results))
-        {
-            $address_data = $address_json->results[0]->address_components;
-        }
+		// If no exact match found, use the first as a closest option
+		if(!isset($address_data) &&
+		   !empty($address_json->results))
+		{
+			$address_data = $address_json->results[0]->address_components;
+		}
 
-        // If still nothing, return empty array
-        if(!isset($address_data))
-            return array();
+		// If still nothing, return empty array
+		if(!isset($address_data))
+			return array();
 
 		// Initialize all values as empty
 		$street_number = '';
