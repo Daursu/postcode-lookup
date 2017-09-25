4600ad45de9924cd564366a318995c8c013feab4
Andrew Westrope <andrew@warwickshire.it>
2017-09-25 18:09:15 +0100

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -2,15 +2,15 @@
 
 return array(
     /*
-  	|--------------------------------------------------------------------------
-  	| API Key
-  	|--------------------------------------------------------------------------
-  	|
-	  | The api key provided by Google. If you don't know your API key
-	  | you can generate one here for Google Maps Geocoding API:
-	  | "https://console.developers.google.com/apis/credentials"
-	  |
-	  */
+    |--------------------------------------------------------------------------
+    | API Key
+    |--------------------------------------------------------------------------
+    |
+    | The api key provided by Google. If you don't know your API key
+    | you can generate one here for Google Maps Geocoding API:
+    | "https://console.developers.google.com/apis/credentials"
+    |
+    */
     
     'apikey' => null
 );
