fdb24316877acf8a114e8bd41537360632458e55
Andrew Westrope <andrew@warwickshire.it>
2017-09-25 18:05:32 +0100

    Create config.php
    Include apikey in config file

---
+++ src/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    /*
+  	|--------------------------------------------------------------------------
+  	| API Key
+  	|--------------------------------------------------------------------------
+  	|
+	  | The api key provided by Google. If you don't know your API key
+	  | you can generate one here for Google Maps Geocoding API:
+	  | "https://console.developers.google.com/apis/credentials"
+	  |
+	  */
+    
+    'apikey' => null
+);
